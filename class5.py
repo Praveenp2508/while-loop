861f9b5bf126bc489f3b67ffb344183d639121a1
Praveen P <107240891+Praveenp2508@users.noreply.github.com>
2024-09-25 09:13:30 +0530

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,33 @@
+#loop
+
+#while loop
+i=1             #initalisation
+while i<10:     #condition
+    print(i, end='')    #statement
+    i=i+1       #increment
+
+j=1
+while j<5:
+    print('j=',j)
+    j=j+1
+
+a=1
+while a<11:
+    print('5','x',a,'=',a*5)
+    a+=1
+
+a=int(input('enter the number'))
+t=1
+while t<11:
+    print(a,'x',t,'=',t*a)
+    t+=1
+
+a=1
+while a<21:
+    print('square of' ,a,'is',a**2)
+    a+=1
+
+a=1
+while a<21:
+    print('cube of' ,a,'is',a**3)
+    a+=1
